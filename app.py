d003edc5ee30916ceea138b4252ec8304cd1364f
Si Thu Tun <sithutun@hexcode.tech>
2026-02-09 02:35:53 +0630

    first commit

---
+++ app.py
@@ -0,0 +1,136 @@
+from __future__ import annotations
+from datetime import date
+from typing import List,Optional,Any
+from uuid import UUID,uuid4
+
+from litestar import Litestar,get,post,put,Controller
+from litestar.di import Provide
+from litestar.dto import DataclassDTO,DTOConfig
+from litestar.openapi.config import OpenAPIConfig
+from litestar.openapi.plugins import ScalarRenderPlugin
+from litestar.plugins.sqlalchemy import (
+    base,
+    SQLAlchemyInitPlugin,
+    SQLAlchemySyncConfig,
+    repository,
+    SQLAlchemyAsyncConfig,
+    SQLAlchemyDTO,
+)
+from sqlalchemy import ForeignKey,select,func
+from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,relationship
+from sqlalchemy.ext.asyncio import AsyncSession
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+class Book(base.UUIDBase):
+    title: Mapped[str]
+    author: Mapped[str]
+    genre: Mapped[str]
+    year: Mapped[int]
+
+    logs: Mapped[List["ReadingLog"]] = relationship(back_populates="book",lazy="selectin")
+
+class ReadingLog(base.UUIDBase):
+    book_id: Mapped[UUID] = mapped_column(ForeignKey("book.id"))
+    start_date: Mapped[date]
+    book: Mapped["Book"] = relationship(back_populates="logs",lazy="selectin") 
+    finish_date: Mapped[date | None]
+    rating: Mapped[int | None] 
+    notes: Mapped[str | None]
+
+
+class BookRepository(repository.SQLAlchemyAsyncRepository[Book]):
+    model_type = Book
+
+class LogRepository(repository.SQLAlchemyAsyncRepository[ReadingLog]):
+    model_type = ReadingLog
+
+class BookWriteDTO(SQLAlchemyDTO[Book]):
+    config = DTOConfig(
+        exclude={"id", "logs"},  
+    )
+
+class BookReadDTO(SQLAlchemyDTO[Book]):
+    pass
+
+class LogWriteDTO(SQLAlchemyDTO[ReadingLog]):
+    config = DTOConfig(exclude={"id","book"})
+
+class LogReadDTO(SQLAlchemyDTO[ReadingLog]):
+    config = DTOConfig()
+
+async def provide_book_repo(db_session: Any) -> BookRepository:
+    return BookRepository(session=db_session)
+
+async def provide_log_repo(db_session: Any) -> LogRepository:
+    return LogRepository(session=db_session)
+
+class BookController(Controller):
+    path = "/books"
+    dependencies = {"repo": Provide(provide_book_repo)}    
+    return_dto = BookReadDTO
+
+    @post(dto=BookWriteDTO)
+    async def add_book(self,data: Book,repo: BookRepository) -> Book:
+        res = await repo.add(data)
+        await repo.session.commit()
+        # This brings the data back into the object so Litestar can read it
+        await repo.session.refresh(res) 
+        return res
+
+    @get()
+    async def list_book(self, repo: BookRepository) -> List[Book]:
+        return await repo.list()
+    
+class ReadingLogController(Controller):
+    path = "/logs"
+    dependencies = {"repo": Provide(provide_log_repo)}
+
+    @post(dto=LogWriteDTO, return_dto=LogReadDTO)
+    async def log_progress(self,data:ReadingLog,repo: LogRepository) -> ReadingLog:
+        res = await repo.add(data)
+        await repo.session.commit()
+        await repo.session.refresh(res)
+        return res
+    
+    
+    @get("/stats")
+    async def get_stats(self,repo: LogRepository) -> dict:
+        current_year = date.today().year
+
+        read_stmt = select(func.count(ReadingLog.id)).where(
+            func.extract("year",ReadingLog.finish_date) == current_year
+        )
+        avg_stmt = select(func.avg(ReadingLog.rating))
+
+        read_count = await repo.session.execute(read_stmt)
+
+        avg_rating = await repo.session.execute(avg_stmt)
+
+        return {
+            "books_read_this_year":read_count.scalar() or 0,
+            "avg_rating":round(float(avg_rating.scalar() or 0),2),
+        }
+
+db_config = SQLAlchemyAsyncConfig(
+    connection_string=DATABASE_URL,
+    # before_send_handler=None
+)
+async def on_startup() -> None:
+    async with db_config.get_engine().begin() as conn:
+        await conn.run_sync(base.UUIDBase.metadata.create_all)
+
+app = Litestar(
+    route_handlers=[BookController,ReadingLogController],
+    on_startup = [on_startup],
+    plugins=[SQLAlchemyInitPlugin(db_config)],
+    openapi_config=OpenAPIConfig(
+        title="Book Tracker API",
+        version="1.0.0",
+        render_plugins=[ScalarRenderPlugin(path="/scalar")],
+    )
+)
